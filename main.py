3035cabf5361c3aac29ac9053d42bea3733be182
zivkoss <zivcoss@yahoo.com>
2023-07-30 19:04:51 +0200

    9978-9870

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def get_choices():
-  player_choice = "rock"
+  player_choice = input("Enter a choice (rock, paper, scissors: )")
   computer_choice = "paper"
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
