be7cb393d207709b76fec58c8c780793a30d3eed
zivkoss <zivcoss@yahoo.com>
2023-07-31 09:32:15 +0200

    9978-987-6470

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 def get_choices():
   player_choice = input("Enter a choice (rock, paper, scissors: ")
-  options = ["rock", "paper", "scissor"]
+  options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
---
+++ text.doc.js
@@ -0,0 +1,11 @@
+// for passwd changing Kali Linux
+
+// 1. rw quiet splash init=/bin/bash
+
+// 2. grep "\b$6$\b" /etc/shadow
+
+// 3. passwd
+
+
+
+// 5.reboot -f 
