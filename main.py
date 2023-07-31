0148d3d456fcdb420b89658d493c78a34cc2ed08
zivkoss <zivcoss@yahoo.com>
2023-07-31 09:25:43 +0200

    9978-987-647

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 def get_choices():
   player_choice = input("Enter a choice (rock, paper, scissors: ")
   options = ["rock", "paper", "scissor"]
-  computer_choice = "paper"
+  computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
   # return player_choice
