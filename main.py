14fd0b7cb6fdfe8b05e01419db083280ef58dc13
zivkoss <zivcoss@yahoo.com>
2023-08-06 09:39:38 +0200

    9978-980979543

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 # for i in range(997):
 #     print(i)
 
-def get_alphabets(startletter, stopletter, step):
- for c in range(ord(startletter.lower()),
-ord(stopletter.lower()), step):
-  yield chr(c)
+# def get_alphabets(startletter, stopletter, step):
+#  for c in range(ord(startletter.lower()),
+# ord(stopletter.lower()), step):
+#   yield chr(c)
 
-print(list(get_alphabets("a", "h", 1)))           
+# print(list(get_alphabets("a", "h", 1)))           
 
-
-
+#=====================================================================================
+# str1 = "Python Learning"
+# str2 = str1[:5]*2 + str1[:4]
+# str3 = str2[:5//2]*3
+# print(str3)
+#===========================================================================================
 
 
 
