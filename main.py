eb23b7ac280ae37d8ea29d38223e8c3ba2bb115e
Nemanja Zivanovic <zivkoss@yahoo.com>
2025-03-25 10:35:28 +0100

    new text

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # def get_alphabets(startletter, stopletter, step):
 #  for c in range(ord(startletter.lower()),
 # ord(stopletter.lower()), step):
-#   yield chr(c)
+#   yield chr(c) //
 
 # print(list(get_alphabets("a", "h", 1)))           
 
