650a0b74df55c035e66e207b5a1af0032d017bc0
Nemanja Zivanovic <zivkoss@yahoo.com>
2025-05-18 08:33:28 +0200

    new text

--- main.py
+++ main.py
@@ -31,10 +31,10 @@
 
 # ////////
 
-rows = 5
-for i in range(rows, 0, -1):
-    print(' ' * (rows - i) +
-          '*' * (2 * i - 1))
+# rows = 5
+# for i in range(rows, 0, -1):
+#     print(' ' * (rows - i) +
+#           '*' * (2 * i - 1))
     # //
 
 
